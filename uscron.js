181e6ce3eb8f60ee7f66c4ad4cc7625e487599ec
Fatih Soydan <fatihsoydan@fatihsoydan.com>
2015-08-03 01:28:26 +0300

    very simple example for node-schedule

---
+++ uscron.js
@@ -0,0 +1,25 @@
+var 
+	schedule 	= require('node-schedule'),
+	spawn 		= require('child_process').spawn,
+	fs 			= require('fs');
+
+
+var register=function(job){
+	schedule.scheduleJob(job.sche,function(){
+		console.log((new Date()).toISOString()+" run "+job.desc);
+		spawn(job.cmd);
+	})
+}
+
+if (fs.existsSync('./cronconf.json')==false){
+	console.log("Tanım dosyası yok \n [cronconf.json] isimli bir dosya üretin\n\nÖrnek içerik :");
+	console.log("{\n\t\"jobs\":[\n\t\t{\n\t\t\t\"desc\" : \"her 10 saniye\",\n\t\t\t\"sche\" : \"*/6 * * * * *\",\n\t\t\t\"cmd\" : \"date\"\n\t\t}\n\t\]\n}");
+} else {
+	var cronconf=JSON.parse(fs.readFileSync('./cronconf.json','utf8'));
+	cronconf.jobs.forEach(function(job){
+		register(job);
+	})	
+}
+
+
+
